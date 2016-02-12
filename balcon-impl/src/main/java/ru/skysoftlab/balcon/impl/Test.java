6deab733ee331eadc0311295bc55d5ad3a3e30de
Lokot <loktionovag@gmail.com>
2016-02-12 22:27:42 +0300

    hot fix

---
+++ balcon-impl/src/main/java/ru/skysoftlab/balcon/impl/Test.java
@@ -0,0 +1,14 @@
+package ru.skysoftlab.balcon.impl;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Integer a = Integer.valueOf(1);
+		Integer b = a;
+		
+		System.out.println(a);
+		System.out.println(b);
+
+	}
+
+}
